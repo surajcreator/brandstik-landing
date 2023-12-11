a1a218c6daaa4b05b851d036ff97b55e87c467a1
surajcreator <suraj.creator@gmail.com>
2023-12-11 21:10:51 +0530

    Updated images for feature

--- src/pages/WelcomeKit/FeatureData.js
+++ src/pages/WelcomeKit/FeatureData.js
@@ -4,42 +4,42 @@ export const featureData = [
       title: "3000+ Range",
       description:
         "Choose from an up-to-date range of 100 readymade kits or curate one from our catalogue of over 3000 products, gift cards/vouchers and experiences. We can also source unique products basis your needs, over and above this range.",
-      img: "/Cork-laptop-sleeve.jpg",
+      img: "/3000+Range.jpg",
     },
     {
       id: "pf24",
       title: "Customization",
       description:
         "This allows clients to customize their gifts with their own branding and messages and have the convenience of storing the gifts with BrandSTIK before distribution. This streamlines the gifting process and allows for efficient and effective corporate gifting solutions.",
-      img: "/5.jpg",
+      img: "/Custimization.jpg",
     },
     {
       id: "pf25",
       title: "Inventory and Warehousing",
       description:
         "We can stock, pack, and ship products as and when orders are placed, eliminating the need for clients to carry inventory at their end. This allows clients to focus on other aspects of their business while BrandSTIK handles the logistics of the gifting process, ensuring timely delivery and hassle-free management of the inventory.",
-      img: "/coconut-shell-collection.jpg",
+      img: "/InventryandWarehousing.jpg",
     },
     {
       id: "pf26",
       title: "Pan India and Global Shipping",
       description:
         "We offer pan India and international shipping services to clients through our network of 13 courier partners. We are experienced in managing shipping across India and globally, having shipped over 800,000 products last year. This allows clients to reach their employees or clients no matter where they are located and makes the gifting process more convenient for clients.",
-        img: "/Cork-laptop-sleeve.jpg",
+        img: "/PanIndiaandGlobalShipping.jpg",
     },
     {
       id: "pf27",
       title: "Sustainable Range",
       description:
         "We are committed to helping corporations and individuals reduce their environmental footprint by offering a sustainable range of products under our Brand Smaller Footprint Co. We focus on eco-positive, sustainable and made-in-India products which are designed to lower the environmental impact of gifting. By choosing these products, corporations and individuals can make eco-conscious choices and contribute to a more sustainable future.",
-      img: "/5.jpg",
+      img: "/SustainableRange.jpg",
     },
     {
       id: "pf28",
       title: "Super Responsive Team",
       description:
         "This ensures that clients receive quick and effective responses to any questions or concerns they may have. The team is dedicated to providing the best possible service to clients and is supported by experienced management to ensure that all aspects of the corporate gifting process run smoothly. Whether it’s product selection, customization, inventory management, shipping, or any other aspect of corporate gifting, clients can rely on BrandSTIK’s professional and responsive team to meet their needs.",
-        img: "/Cork-laptop-sleeve.jpg",
+        img: "/SuperResponsiveTeam.jpg",
     },
   ];
 
