999bc6a89d5b78f7a41ecb4487b1b91244f8b783
surajcreator <suraj.creator@gmail.com>
2023-12-11 21:22:44 +0530

    testimonial updated

--- src/components/Testimonials/TestimonialData.js
+++ src/components/Testimonials/TestimonialData.js
@@ -4,7 +4,7 @@ export const testimonialData = [
         title: "3000+ Range",
         description:
             "Choose from an up-to-date range of 100 readymade kits or curate one from our catalogue of over 3000 products, gift cards/vouchers and experiences. We can also source unique products basis your needs, over and above this range.",
-        img: "/testi-one.jpg",
+        img: "/testi-one.jpeg",
     },
     {
         id: "pf101",
